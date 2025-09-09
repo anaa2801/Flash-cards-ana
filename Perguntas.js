f5c43fbb6c2bf54933617a98d4f4176bd80104ce
anaa2801 <alberton.ana@escola.pr.gov.br>
2025-09-09 08:46:29 -0300

    09/09

---
+++ Perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'Biologia',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados em uma doação de sangue?',
+    'Entre 4 e 6 litros'
+)
+
+criaCartao(
+    'História',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos temos no nosso corpo?',
+    '206  ossos'
+)
+
+criaCartao(
+    'arte',
+    ' Quem pintou Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'química',
+    'Qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
+
+criaCartao(
+    'química',
+    'Qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
+
+criaCartao(
+    'química',
+    'Qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
+
+criaCartao(
+    'química',
+    'Qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
+
+criaCartao(
+    'química',
+    'Qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
+
+criaCartao(
+    'química',
+    'Qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
+
+criaCartao(
+    'química',
+    'Qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
